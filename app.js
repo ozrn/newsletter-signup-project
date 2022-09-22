f689e67421e1f55e920acaa2b0ba3ad1e6597746
ozrn <ozerenkevser92@gmail.com>
2022-09-22 14:53:46 +0300

    Install and configure mailchimp module

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const mailchimp = require("@mailchimp/mailchimp_marketing"); // to require mailchimp_marketing !!
 
 const app = express();
+
 app.use(express.static("public")); // we need to use express.static fn to serve up static files
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
 })
 
+mailchimp.setConfig({
+
+  apiKey: "ae1c785c3494102f26b92c1d51466803-us13",
+
+  server: "us13"
+
+});
+
+app.post("/", (req,res) => {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  const listId = "fbb15baff9";
+
+  const subscribingUser = {
+
+    firstName: firstName,
+
+    lastName: lastName,
+
+    email: email;
+
+  };
+})
 
 app.listen(8000,() =>{
   console.log("Server is running on 8000");
 })
+
+// API KEY = ae1c785c3494102f26b92c1d51466803-us13
+// list id = fbb15baff9
