1331ece5508327c183a02fd6c31655e9554bb905
ozrn <ozerenkevser92@gmail.com>
2022-09-22 14:07:42 +0300

    Create folder for static files, render them

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+app.use(express.static("public")); // we need to use express.static fn to serve up static files
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
